77ed14b3c14578f9c3d74844110683ce24df0196
andrewkandzuba <andrey.kandzuba@gmail.com>
2024-05-08 10:53:50 -0500

    Brand Console Application is complete

---
+++ src/band/main.py
@@ -0,0 +1,13 @@
+
+
+def main():
+    print("Welcome to the Name Band!")
+    print("What is a name of the city you grew up in?")
+    city = input()
+    print("What is the name of your pet?")
+    pet = input()
+    print("Your band name could be " + city + " " + pet)
+
+
+if __name__ == "__main__":
+    main()
